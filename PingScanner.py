b8016a7e08f9608c34cfd30821534ab0ee7a7791
clement <clement.arthaud-berthet@eleve.isep.fr>
2023-03-08 14:52:40 +0100

    Same mais avec ping en simult

--- PingScanner.py
+++ PingScanner.py
@@ -1,20 +1,24 @@
+import concurrent.futures
 import subprocess
 import time
 
-# Liste d'adresses IP à pinguer
 ip_list = ["104.110.240.88", "18.66.112.92", "10.0.0.1"]
 
-# Fonction pour effectuer un ping et retourner True si l'hôte est accessible, False sinon
 def ping(ip_address):
     res = subprocess.call(["ping", "-c", "1", "-t", "200", ip_address], stdout=subprocess.PIPE)
     return res == 0
 
-# Boucle infinie pour pinger en continu
+def ping_ips(ip_list):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = {executor.submit(ping, ip): ip for ip in ip_list}
+        for future in concurrent.futures.as_completed(results):
+            ip = results[future]
+            if future.result():
+                print(f"{ip} is UPPPP")
+            else:
+                print(f"{ip} is down")
+
 while True:
     print("Scanning...")
-    for ip_address in ip_list:
-        if ping(ip_address):
-            print(f"{ip_address} is up")
-        else:
-            print(f"{ip_address} is down")
-    time.sleep(5) # Attendre 5 secondes avant de repinger
+    ping_ips(ip_list)
+    time.sleep(5)
