688b9d2de9d5e311d9e9ad9ba2828eb0df038e8e
clement <clement.arthaud-berthet@eleve.isep.fr>
2023-03-09 11:47:42 +0100

    works in terminal

--- PingScanner.py
+++ PingScanner.py
@@ -1,24 +1,16 @@
-import concurrent.futures
 import subprocess
 import time
-
-ip_list = ["104.110.240.88", "18.66.112.92", "10.0.0.1"]
 
 def ping(ip_address):
     res = subprocess.call(["ping", "-c", "1", "-t", "200", ip_address], stdout=subprocess.PIPE)
     return res == 0
 
-def ping_ips(ip_list):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = {executor.submit(ping, ip): ip for ip in ip_list}
-        for future in concurrent.futures.as_completed(results):
-            ip = results[future]
-            if future.result():
-                print(f"{ip} is UPPPP")
-            else:
-                print(f"{ip} is down")
-
+ip_addresses = input("Enter IP addresses to ping (separated by spaces): ").split()
 while True:
     print("Scanning...")
-    ping_ips(ip_list)
+    for ip_address in ip_addresses:
+        if ping(ip_address):
+            print(f"{ip_address} is UP")
+        else:
+            print(f"{ip_address} is down")
     time.sleep(5)
